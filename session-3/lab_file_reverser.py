a6c79ad190b1ef202a5f4336c95d717809e5726b
Lionel Amarado <lamarado@innovuze.com>
2020-03-25 00:19:49 +0800

    Save lab exercises

--- session-3/lab_file_reverser.py
+++ session-3/lab_file_reverser.py
@@ -1,15 +1,51 @@
-import csv
-import json
+import argparse
+import sys
 
-with open('sample_products.csv', newline='') as f:
-    reader = csv.reader(f)
-    for row in reader:
-        print(row)
+# create an instance of ArgumentParser without any arguments
+parser = argparse.ArgumentParser()
 
-#with open('/temp/file.csv', 'w', newline='') as f:
-#    writer = csv.writer(f)
-#    writer.writerows(iterable)
+# use the add_argument method to specify a positional argument called filename and
+# provide some help text using the help argument.
+parser.add_argument('filename', help='the file to read')
 
-with open('/Users/lamarado/Downloads/Keys/gpayroll-964-490e9a520891.json', 'r') as f:
-    data = json.load(f)
-    print(data)
+# add a --limit flag
+# - to specify that an argument is a flag, we need to place two hyphens at the
+#   beginning of the flag's name
+# - we specified a shorter version of the flag as our second argument
+# - we used the type option for add_argument to state that we want the value
+#   converted to an integer
+parser.add_argument('--limit', '-l', type=int, help='the number of lines to read')
+
+# add a --version flag
+# - use the action option to specify a string to print out when this flag is received
+parser.add_argument('--version', '-v', action='version', version='%(prog)s 1.0')
+
+# tell the parser to parse the arguments from stdin using the parse_args method and
+# store the parsed arguments as to the variable args
+args = parser.parse_args()
+
+# We utilize the try statement to denote that it's quite possible for an error
+# to happen within the try block
+try:
+    f = open(args.filename)
+    limit = args.limit
+# We can handle specific types of errors using the except keyword (we can have more than one)
+except FileNotFoundError as err:
+    print(f"Error: {err}")
+    # set exit status to 1 to indicate error
+    sys.exit(1)
+# If there isn't an error, we want to carry out the code that is in the else block
+else:
+# the actual business logic for reversing the contents of the file
+    with open(args.filename) as f:
+        lines = f.readlines()
+        lines.reverse()
+    
+        if args.limit:
+            lines = lines[:args.limit]
+
+        for line in lines:
+            print(line.strip()[::-1])
+
+# If we want to execute some code regardless of there being an error or not,
+# we can put that in a finally block at the very end of our try/except code
