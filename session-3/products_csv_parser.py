12e54f3a0ba83c81d3e222ea53a020ec58f4428d
Lionel Amarado <lamarado@innovuze.com>
2020-03-28 18:21:01 +0800

    Fix flake8 warnings

--- session-3/products_csv_parser.py
+++ session-3/products_csv_parser.py
@@ -3,15 +3,15 @@
 # Exercise: Products CSV Reader and Writer
 
 # The main objective of this project idea is to read a CSV with a list of
-#  products for an ecommerce site. The application should be able to:
+# products for an ecommerce site. The application should be able to:
 
 # - Remove all products that don’t have categories
 # - Output a new CSV file that contains all products that have categories
-# - Use argparse for passing the filename of the CSV input file 
+# - Use argparse for passing the filename of the CSV input file
 # - Prompt the user to specify the filename of the CSV output file
 # - Name the exercise file products_csv_parser.py
 # - Fix flake8 errors (if you don’t know how to do this yet, refer to
-#  How to Use Flake8 Guide)
+#   How to Use Flake8 Guide)
 
 import argparse
 import csv
@@ -36,7 +36,6 @@ except FileNotFoundError as err:
     sys.exit(ERROR_RETURN)
 else:
     valid_products = []
-    
     with open(args.filename, newline='') as f:
         reader = csv.reader(f)
         rows = list(map(tuple, reader))
@@ -45,4 +44,4 @@ else:
     with open(output_filename, 'w', newline='') as f:
         writer = csv.writer(f)
         writer.writerows(valid_products)
-        print(f"Valid products are saved on the following file: {output_filename}")
+        print(f"Output saved on {output_filename}.")
